e1d1376436c26a62b153a70d4cd26c7eac950e44
aleks <aleksmail85@mail.ru>
2015-09-27 23:17:23 +0300

    [+] add websocket client endpoint [+} add slf4j logger

--- src/main/java/ru/testing/client/console/ConsoleApp.java
+++ src/main/java/ru/testing/client/console/ConsoleApp.java
@@ -1,12 +1,19 @@
 package ru.testing.client.console;
 
 import com.beust.jcommander.JCommander;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import ru.testing.client.config.Configuration;
+import ru.testing.client.websocket.Client;
+import java.net.URI;
+import java.util.Scanner;
 
 /**
  * Console testing client
  */
 public class ConsoleApp {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(ConsoleApp.class);
 
     public static void main(String[] args) {
         Configuration config = new Configuration();
@@ -24,6 +31,26 @@ public class ConsoleApp {
         if (config.isHelp()) {
             parser.usage();
             System.exit(0);
+        }
+
+        try {
+            final Client client = new Client(new URI("ws://echo.websocket.org"));
+            String message;
+            client.addMessageHandler(serverMessage -> LOGGER.info(String.format("=> %s", serverMessage)));
+
+            while (true) {
+                Scanner scanner = new Scanner(System.in);
+                System.out.print("Send message: ");
+                message = scanner.nextLine();
+                if (message.equals("exit")) {
+                    client.getSession().close();
+                    break;
+                }
+                client.sendMessage(message);
+                Thread.sleep(2000);
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
         }
     }
 }
--- src/main/java/ru/testing/client/websocket/Client.java
+++ src/main/java/ru/testing/client/websocket/Client.java
@@ -1,8 +1,66 @@
 package ru.testing.client.websocket;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import javax.websocket.*;
+import java.net.URI;
+
 /**
  * WebSocket client
  */
+@ClientEndpoint
 public class Client {
 
+    private static final Logger LOGGER = LoggerFactory.getLogger(Client.class);
+    private Session session = null;
+    private MessageHandler messageHandler;
+
+    public Client(final URI endpointURI) {
+        try {
+            WebSocketContainer container = ContainerProvider.getWebSocketContainer();
+            container.connectToServer(this, endpointURI);
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    @OnOpen
+    public void onOpen(final Session session) {
+        if (session.isOpen()) {
+            LOGGER.info(String.format("Connection open with server %s", session.getRequestURI()));
+            this.session = session;
+        }
+    }
+
+    @OnClose
+    public void onClose(final Session session, final CloseReason reason) {
+        if (session.isOpen()) {
+            LOGGER.info(String.format("Connection close: %s", reason));
+            this.session = null;
+        }
+    }
+
+    @OnMessage
+    public void onMessage(final String message) {
+        if (messageHandler != null) {
+            messageHandler.handleMessage(message);
+        }
+    }
+
+    public void addMessageHandler(final MessageHandler msgHandler) {
+        messageHandler = msgHandler;
+    }
+
+    public void sendMessage(final String message) {
+        session.getAsyncRemote().sendText(message);
+    }
+
+    public static interface MessageHandler {
+        void handleMessage(String message);
+    }
+
+    public Session getSession() {
+        return this.session;
+    }
 }
