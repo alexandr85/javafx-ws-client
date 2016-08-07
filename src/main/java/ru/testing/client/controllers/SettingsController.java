569b76063353af6c44d1bc864a09913a148cb989
aleks <aleksmail85@mail.ru>
2016-08-07 13:01:35 +0300

    [~] fix ui

--- src/main/java/ru/testing/client/controllers/SettingsController.java
+++ src/main/java/ru/testing/client/controllers/SettingsController.java
@@ -1,11 +1,38 @@
 package ru.testing.client.controllers;
 
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+import javafx.scene.control.Slider;
+
 /**
  * Controller for settings tab form
  */
 public class SettingsController {
 
+    private int fontValue;
+
+    @FXML
+    private Label fontValueLabel;
+    @FXML
+    private Slider fontValueSlider;
+
     public SettingsController(MainController mainController) {
 
+    }
+
+    @FXML
+    private void initialize() {
+        fontValueSlider.valueProperty().addListener(observable -> {
+            setFontValue(((Number) fontValueSlider.getValue()).intValue());
+            fontValueLabel.setText(String.format("Font size: %spx", getFontValue()));
+        });
+    }
+
+    public int getFontValue() {
+        return fontValue;
+    }
+
+    public void setFontValue(int fontValue) {
+        this.fontValue = fontValue;
     }
 }
